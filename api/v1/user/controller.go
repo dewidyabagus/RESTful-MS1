c6467e58ad606b289d911955024e872a6f8c1df9
Widya Ade Bagus <dewidyabagus@gmail.com>
2021-11-07 10:51:01 +0700

    response adjustment using common features

--- api/v1/user/controller.go
+++ api/v1/user/controller.go
@@ -1,9 +1,9 @@
 package user
 
 import (
+	"RESTfulMS1/api/common"
 	"RESTfulMS1/api/v1/user/request"
 	"RESTfulMS1/business/user"
-	"net/http"
 
 	echo "github.com/labstack/echo/v4"
 )
@@ -23,13 +23,13 @@ func (c *Controller) AddNewUser(ctx echo.Context) error {
 
 	err = ctx.Bind(user)
 	if err != nil {
-		return ctx.JSON(http.StatusBadRequest, map[string]string{"message": "bad request 1"})
+		return ctx.JSON(common.BadRequestResponse())
 	}
 
 	err = c.service.AddNewUser(user.ToUserAddSpec())
 	if err != nil {
-		return ctx.JSON(http.StatusBadRequest, map[string]string{"message": "bad request 2"})
+		return ctx.JSON(common.NewBusinessErrorResponse(err))
 	}
 
-	return ctx.JSON(http.StatusOK, map[string]string{"message": "success"})
+	return ctx.JSON(common.SuccessResponseWithoutData())
 }
